be22e865a7a3f29fb1c662661f8c1edb9942215b
Kemyke <kemyyy@gmail.com>
2015-12-08 23:55:50 +0100

    Add Common project Add ServiceHost project

---
+++ SapSoapCardWriter.Common/SapSoapCardWriterConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapSoapCardWriter.Common
+{
+    public sealed class SapSoapCardWriterConfig : ConfigurationSection, ISapSoapCardWriterConfig
+    {
+        //[ConfigurationProperty("Sample", IsRequired = true)]
+        //public string Sample
+        //{
+        //    get { return (string)this["Sample"]; }
+        //    set { this["Sample"] = value; }
+        //}
+    }
+}
