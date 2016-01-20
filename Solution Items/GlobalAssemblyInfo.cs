21749e29cffe611cdd4fc48e83e89f6913ea8c97
Kemyke <kemyyy@gmail.com>
2016-01-20 20:27:09 +0100

    Add assembly info

---
+++ Solution Items/GlobalAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Dvcon")]
+[assembly: AssemblyProduct("Smartcard Writer")]
+[assembly: AssemblyCopyright("Copyright © Dvcon 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.6-Undefined")]
