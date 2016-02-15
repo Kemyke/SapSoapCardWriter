6433a6cf280cc9e48bc01e328a2642a37ee42880
Kemyke <kemyyy@gmail.com>
2016-02-15 23:26:39 +0100

    Fix config transform add urlacl

--- SapSoapCardWriter.ServiceHost/ProjectInstaller.cs
+++ SapSoapCardWriter.ServiceHost/ProjectInstaller.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,6 +15,53 @@ namespace SapSoapCardWriter.ServiceHost
         public ProjectInstaller()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAfterInstall(IDictionary savedState)
+        {
+            base.OnAfterInstall(savedState);
+
+            var cmd = "netsh http add urlacl url=http://+:10132/ user=\"NETWORK SERVICE\"";
+            var processInfo = new ProcessStartInfo
+            {
+                Verb = "runas",
+                FileName = "cmd",
+                Arguments = "/c " + cmd
+            };
+            Process.Start(processInfo);
+
+            cmd = "netsh http add urlacl url=https://+:10134/ user=\"NETWORK SERVICE\"";
+            processInfo = new ProcessStartInfo
+            {
+                Verb = "runas",
+                FileName = "cmd",
+                Arguments = "/c " + cmd
+            };
+            Process.Start(processInfo);
+        }
+
+        protected override void OnAfterUninstall(IDictionary savedState)
+        {
+            base.OnAfterUninstall(savedState);
+
+            var cmd = "netsh http delete urlacl url=http://+:10132/";
+            var processInfo = new ProcessStartInfo
+            {
+                Verb = "runas",
+                FileName = "cmd",
+                Arguments = "/c " + cmd
+            };
+            Process.Start(processInfo);
+
+            cmd = "netsh http delete urlacl url=https://+:10134/";
+            
+            processInfo = new ProcessStartInfo
+            {
+                Verb = "runas",
+                FileName = "cmd",
+                Arguments = "/c " + cmd
+            };
+            Process.Start(processInfo);
         }
     }
 }
