624ae810026a8c86095885bc027868d1b8d10c85
Kemyke <kemyyy@gmail.com>
2017-03-22 21:03:36 +0100

    Use global assembly info

--- SapSoapCardWriter.GUI/Properties/AssemblyInfo.cs
+++ SapSoapCardWriter.GUI/Properties/AssemblyInfo.cs
@@ -8,29 +8,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SapSoapCardWriter.GUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SapSoapCardWriter.GUI")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("296209fc-1852-424f-8769-4c93a0bc8a62")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9.0")]
-[assembly: AssemblyFileVersion("1.0.9.0")]
