803f9bb766e2953a6d182a8039910268004b1735
Kemyke <kemyyy@gmail.com>
2017-09-27 21:24:27 +0200

    Bump version

--- Solution Items/GlobalAssemblyInfo.cs
+++ Solution Items/GlobalAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.10.0")]
-[assembly: AssemblyInformationalVersion("1.0.10.0")]
+[assembly: AssemblyVersion("1.0.11.0")]
+[assembly: AssemblyInformationalVersion("1.0.11.0")]
